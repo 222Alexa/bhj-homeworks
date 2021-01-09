94c9d54de82cdb51dfc0cde0c0a99450213b654f
Alexandra Titova <auto-steklo@mail.ru>
2021-01-09 04:02:21 +0300

    first commit

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,2 +1,17 @@
 'use strict';
-
+const modal = document.getElementById('subscribe-modal');
+const btnClose = document.querySelector('.modal__close');
+btnClose.addEventListener('click', () => {
+    modal.classList.remove('modal_active');
+    document.cookie = 'status = active';
+    alert(document.cookie);
+});
+window.addEventListener('DOMContentLoaded', () => {
+    if (!document.cookie.includes('status = active')) {
+        modal.classList.add('modal_active');
+    }
+    else {
+        document.cookie = '';
+        return;
+    }
+});
